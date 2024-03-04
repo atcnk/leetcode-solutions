185bf5e0251ccaacc2c0afe78b01ca3ba3ebaca3
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-04 17:11:53 +0300

    Added solution of 1431. Kids With the Greatest Number of Candies

---
+++ StudyPlan_LeetCode75/1431_KidsWithTheGreatestNumberOfCandies.cs
@@ -0,0 +1,14 @@
+﻿/* find greatest candie number in candies array
+ * loop through array, select if (candie + extra candie) >= (greatest candie)
+ * convert selected items to list and return
+ */
+
+public class Solution
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        int gc = candies.Max();
+
+        return candies.Select(c => (c + extraCandies) >= gc).ToList();
+    }
+}
