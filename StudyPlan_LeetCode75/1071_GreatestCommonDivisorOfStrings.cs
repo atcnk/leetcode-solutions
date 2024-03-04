bd1dc302288372e4d91a05ce2ee489068051ca21
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-04 17:05:35 +0300

    Added solution of 1071. Greatest Common Divisor of Strings

---
+++ StudyPlan_LeetCode75/1071_GreatestCommonDivisorOfStrings.cs
@@ -0,0 +1,31 @@
+﻿/* check if str1 + str2 == str2 + str1
+ * if not, there is no common part, return empty string
+ * if true, consider strings lengthes as numbers
+ * do the math to find gdc for these two numbers
+ * substring str1 or str2 with (0, gdc) indexes, return it
+ */
+
+public class Solution
+{
+    public string GcdOfStrings(string str1, string str2)
+    {
+        StringBuilder sb1 = new(str1 + str2);
+        StringBuilder sb2 = new(str2 + str1);
+
+        if (!sb1.Equals(sb2))
+        {
+            return "";
+        }
+
+        int n1 = str1.Length, n2 = str2.Length, t;
+
+        while (n2 != 0)
+        {
+            t = n2;
+            n2 = n1 % n2;
+            n1 = t;
+        }
+
+        return sb1.ToString(0, n1);
+    }
+}
