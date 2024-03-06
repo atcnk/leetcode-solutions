14cc12f60c83fd8d4bce7531a06b0157e96dff96
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-06 23:37:59 +0300

    Added solution of 334. Increasing Triplet Subsequence

---
+++ StudyPlan_LeetCode75/334_IncreasingTripletSubsequence.cs
@@ -0,0 +1,35 @@
+﻿/* if nums length <= 2 its impossible to i < j < k exist, return false
+ * let integer f (first) and s (second) to be max value
+ * loop through nums
+ * compare first f then s with the number, if n <= assign it
+ * if there something like n > f and n > s then we have f < s < n, return true
+ * if not return false
+ */
+
+public class Solution
+{
+    public bool IncreasingTriplet(int[] nums)
+    {
+        if (nums.Length <= 2) return false;
+
+        int f = Int32.MaxValue, s = f;
+
+        foreach (var n in nums)
+        {
+            if (n <= f)
+            {
+                f = n;
+            }
+            else if (n <= s)
+            {
+                s = n;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
