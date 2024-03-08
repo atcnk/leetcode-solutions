bfca5e047df4e6a329b4cac5371e3eddb9b8e94d
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-08 23:54:06 +0300

    Added solution of 283. Move Zeroes

---
+++ StudyPlan_LeetCode75/283_MoveZeroes.cs
@@ -0,0 +1,22 @@
+﻿class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int index = -1;
+        int firstIndexOfZero = 0;
+
+        while (++index < nums.length)
+        {
+            if (nums[index] == 0) continue;
+
+            if (index == firstIndexOfZero)
+            {
+                firstIndexOfZero++;
+                continue;
+            }
+
+            nums[firstIndexOfZero++] = nums[index];
+            nums[index] = 0;
+        }
+    }
+}
