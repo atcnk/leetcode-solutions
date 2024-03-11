f696fbdb4efe37e7b6225c526f8df42fd545b226
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-11 23:36:25 +0300

    Added solution of 643. Maximum Average Subarray I

---
+++ StudyPlan_LeetCode75/643_MaximumAverageSubarrayI.cs
@@ -0,0 +1,27 @@
+﻿/* declare sum (s) and maxSum (ms)
+ * calculate sum of first k numbers
+ * logic is add next number of array and subtract first, like we're swiping
+ * start loop from index k, add number at index i and subtract number at index i - k
+ * compare if s bigger than ms
+ * return ms / k 
+ */
+
+public class Solution
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        int s = 0, ms = 0, nl = nums.Length;
+
+        for (int i = 0; i < k; i++) s += nums[i];
+
+        ms = s;
+
+        for (int i = k; i < nl; i++)
+        {
+            s += nums[i] - nums[i - k];
+            ms = Math.Max(ms, s);
+        }
+
+        return (double)ms / k;
+    }
+}
