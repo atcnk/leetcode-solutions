30c7663cb6b6f0f3e5ec085619d0c74d88c177fa
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-13 18:10:36 +0300

    Added solution of 1493. Longest Subarray of 1's After Deleting One Element

---
+++ StudyPlan_LeetCode75/1493_LongestSubarrayOf1SAfterDeletingOneElement.cs
@@ -0,0 +1,22 @@
+﻿/* check the answer of 1004. Max Consecutive Ones III problem
+ * this problem has same logic, there are two differences 
+ * first difference is k is constant and its 1
+ * second difference is we delete 1 element, so we need to decrease answer 
+ * consider like this and read the other answer
+ */
+
+public class Solution
+{
+    public int LongestSubarray(int[] nums)
+    {
+        int k = 1, fi = 0, li = -1, nl = nums.Length;
+
+        while (++li < nl)
+        {
+            if (nums[li] == 0) k--;
+            if (k < 0 && nums[fi++] == 0) k++;
+        }
+
+        return li - fi - 1;
+    }
+}
