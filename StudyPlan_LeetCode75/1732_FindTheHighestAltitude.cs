5b8103b6754245f99c4760e6b3f4caade36aa734
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-17 00:24:23 +0300

    Added solution of 1732. Find the Highest Altitude

---
+++ StudyPlan_LeetCode75/1732_FindTheHighestAltitude.cs
@@ -0,0 +1,26 @@
+﻿/* update a (altitude) every time 
+ * check if a is bigger than ma (max altitude)
+ * return ma
+ */
+
+public class Solution
+{
+    public int LargestAltitude(int[] gain)
+    {
+        int a = 0, ma = 0, i = -1, gl = gain.Length;
+
+        while (++i < gl)
+        {
+            a += gain[i];
+
+            if (a <= ma)
+            {
+                continue;
+            }
+
+            ma = a;
+        }
+
+        return ma;
+    }
+}
