41c79a72bf1fb5e58203830dc45c479e3fffa124
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-19 19:15:48 +0300

    Added solution of find the difference of two arrays

---
+++ StudyPlan_LeetCode75/2215_FindTheDifferenceOfTwoArrays.cs
@@ -0,0 +1,11 @@
+﻿/* solution is creating a new list from nums1 except nums2 numbers and the same for nums2
+ * there is except method for arrays, use it and cast to list
+ */
+
+public class Solution
+{
+    public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+    {
+        return new List<IList<int>> { nums1.Except(nums2).ToList(), nums2.Except(nums1).ToList() };
+    }
+}
