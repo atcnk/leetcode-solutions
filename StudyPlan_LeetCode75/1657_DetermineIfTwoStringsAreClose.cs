70411bb1fe6b078dc0b16cc759a49b71f0d3d2c1
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-03-24 23:22:40 +0300

    Added solution of 1657 Determine If Two Strings Are Close

---
+++ StudyPlan_LeetCode75/1657_DetermineIfTwoStringsAreClose.cs
@@ -0,0 +1,15 @@
+﻿/* get length of strings, if not equal it is impossible to attain these words, return false
+ * 
+ */
+
+public class Solution
+{
+    public bool CloseStrings(string word1, string word2)
+    {
+        int w1l = word1.Length, w2l = word2.Length;
+
+        if (w1l != w2l) return false;
+
+
+    }
+}
